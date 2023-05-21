55895af181c79cf6b12dd64757e75063a2bbc5e0
Eric Kayiranga <tipulive@gmail.com>
2023-05-21 19:47:57 +0200

    Create Laravel Comment API in CommentController

--- LaravelApp/app/Http/Controllers/CommentController.php
+++ LaravelApp/app/Http/Controllers/CommentController.php
@@ -3,8 +3,118 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class CommentController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        date_default_timezone_set(env('TIME_ZONE'));
+        $this->today = date('Y-m-d H:i:s', time());
+
+
+    }
+
+    public function saveComment(Request $request)
+    {
+
+        $check=DB::table("comments")
+        ->insert(
+            [
+            "author_name"=>$request->input("name"),
+            "author_email"=>$request->input("email"),
+            "comment"=>$request->input("comment"),
+            "created_at"=>$this->today
+            ]
+            );
+
+            if($check)
+            {
+             return response([
+
+                 "status"=>true,
+                 "result"=>$check,
+
+             ]);
+            }
+            else{
+             return response([
+
+                 "status"=>false,
+                 "result"=>$check,
+
+             ]);
+            }
+
+
+    }
+    public function readComment(Request $request){
+
+
+
+$startFrom=$request->input("startFrom")??0; // Startfrom limit
+$dataLimit=$request->input("dataLimit")??10; // data limit to be shown
+
+
+
+   $check=DB::select('select *from comments order by id limit :limit offset :offset',array('limit'=>$dataLimit,'offset'=>$startFrom));
+
+   if($check)
+   {
+    return response([
+
+        "status"=>true,
+        "result"=>$check,
+
+    ]);
+   }
+   else{
+    return response([
+
+        "status"=>false,
+        "result"=>$check,
+
+    ]);
+   }
+
+
+
+    }
+
+
+    public function searchComment(Request $request){//by name or by email
+
+
+        $searchInput=$request->input('search');
+        $searchInput = filter_var($searchInput, FILTER_SANITIZE_STRING);
+
+
+
+
+           $check=DB::select("select *from comments where author_name Like '%$searchInput%' or author_email Like '%$searchInput%' limit 10");
+
+               if($check)
+               {
+                return response([
+
+                    "status"=>true,
+                    "result"=>$check,
+
+                ]);
+               }
+               else{
+                return response([
+
+                    "status"=>false,
+                    "result"=>$check,
+
+                ]);
+               }
+
+
+
+            }
+
 }
--- LaravelApp/routes/api.php
+++ LaravelApp/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('saveComment', 'App\Http\Controllers\CommentController@saveComment');
+Route::get('readComment', 'App\Http\Controllers\CommentController@readComment');
+Route::get('searchComment', 'App\Http\Controllers\CommentController@searchComment');
+
