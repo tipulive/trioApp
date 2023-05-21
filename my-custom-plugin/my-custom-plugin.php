8dc00ebacc3536fda5931820c73c7eacf3174a47
Eric Kayiranga <tipulive@gmail.com>
2023-05-21 20:46:45 +0200

    add more details about my plugin

---
+++ my-custom-plugin/my-custom-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: My Custom Plugin
+Plugin URI: https://github.com/tipulive/trioApp/tree/main/my-custom-plugin
+Description: A Simple custom WordPress plugin to save post comment in Wordpress and save data in my Laravel Third party App using my laravel API.
+Version: 1.0
+Author: Eric Kayiranga
+Author URI: https://github.com/tipulive
+*/
+
+// Include the functionality file
+require_once( plugin_dir_path( __FILE__ ) . 'my-custom-plugin-functionality.php' );
